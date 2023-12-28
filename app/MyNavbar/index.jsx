972ae6cb3c7f30a25a3341aa017c049dbfd0688f
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2023-12-27 21:14:30 -0400

    Customizing Navbar

--- app/MyNavbar/index.jsx
+++ app/MyNavbar/index.jsx
@@ -8,7 +8,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Image from 'next/image';
-import styles from "./page.module.css"
+import { user_dropdown, logo_text, btn_search, navbar_icons } from "./navbars.module.css"
 import MySubNavbar from './MySubNavbar';
 import Stack from 'react-bootstrap/Stack';
 
@@ -16,7 +16,7 @@ function MyNavbar() {
     return (
         <Navbar expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#" className={styles.logoText}>
+                <Navbar.Brand href="#" className={logo_text}>
                     <Image
                         alt=""
                         src="/imgs/logo.svg"
@@ -27,7 +27,7 @@ function MyNavbar() {
                     LLDMall
                 </Navbar.Brand>
 
-                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Toggle aria-controls="navbarScroll" className='btn' />
 
                 <Navbar.Collapse id="navbarScroll">
                     <Form className="w-100 border border-opacity-25 border-secondary rounded-5 mx-lg-5 py-1">
@@ -47,16 +47,14 @@ function MyNavbar() {
                                 aria-label="Text input with dropdown button"
                                 className='border-top-0 border-bottom-0 border-end-0'
                             />
-                            <Button className={`${styles.btnSearch} rounded-5 me-1`}>
+                            <Button className={`${btn_search} rounded-5 me-1`}>
                                 <i className="bi bi-search"></i>{' '} Buscar
                             </Button>
                         </InputGroup>
                     </Form>
 
                     <Nav
-                        className="me-auto my-2 my-lg-0 align-items-center gap-2"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
+                        className="me-auto my-2 my-lg-0 align-items-center justify-content-center flex-row gap-sm-5 gap-lg-2 gap-4"
                     >
                         <NavDropdown title="EN" id="navbarScrollingDropdown">
                             <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
@@ -70,24 +68,39 @@ function MyNavbar() {
                         </NavDropdown>
 
                         <Nav.Link href="#action1">
-                            <i className={`${styles.navbar_icons} bi bi-cart`}></i>
+                            <i className={`${navbar_icons} bi bi-cart`}></i>
                         </Nav.Link>
                         <Nav.Link href="#action2">
-                            <i className={`${styles.navbar_icons} bi bi-bell`}></i>
+                            <i className={`${navbar_icons} bi bi-bell`}></i>
                         </Nav.Link>
                         <Nav.Link href="#" >
-                            <i className={`${styles.navbar_icons} bi bi-envelope`}></i>
+                            <i className={`${navbar_icons} bi bi-envelope`}></i>
                         </Nav.Link>
                         |
-                        <Nav.Link href="#" >
-                            <Image
-                                alt=""
-                                src="/imgs/balazs-orban.png"
-                                width="45"
-                                height="45"
-                                className="d-inline-block align-top rounded rounded-circle"
-                            />
-                        </Nav.Link>
+                        <NavDropdown
+                            title={
+                                <Image
+                                    alt=""
+                                    src="/imgs/balazs-orban.png"
+                                    width="45"
+                                    height="45"
+                                    className="d-inline-block align-top rounded rounded-circle"
+                                />
+                            }
+                            id="navbarScrollingDropdown"
+                            className={user_dropdown}
+                            align="end"
+                        >
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Something else here
+                            </NavDropdown.Item>
+                        </NavDropdown>
+
                     </Nav>
 
                 </Navbar.Collapse>
