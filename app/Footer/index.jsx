2a48c8a936fa2617923dd1619aed519840915e06
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2023-12-29 17:11:53 -0400

    Moved to Components folder

--- app/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-
-export default function Footer() {
-    return <div className="p-5">
-        <h6 className="text-muted text-center">&copy; 2024 LLDMAll. All Rights Reserved.</h6>
-    </div>
-}
