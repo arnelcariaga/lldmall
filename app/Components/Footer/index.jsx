9d94ba48396145a6880035cdb16ab9895db03c8f
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2023-12-30 23:35:57 -0400

    Padding modified

--- app/Components/Footer/index.jsx
+++ app/Components/Footer/index.jsx
@@ -2,7 +2,7 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 export default function Footer() {
-    return <Row className="p-5">
+    return <Row className="p-3">
         <Col>
             <h6 className="text-muted text-center">&copy; 2024 LLDMAll. All Rights Reserved.</h6>
         </Col>
