be90e48d519218e43f1545d402868e120b099f9a
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2024-01-06 00:50:31 -0400

    Applying form handler and validation

--- app/(auth)/signIn/page.jsx
+++ app/(auth)/signIn/page.jsx
@@ -6,48 +6,84 @@ import InputGroup from "react-bootstrap/InputGroup"
 import Stack from "react-bootstrap/Stack"
 import Link from "next/link"
 import Image from "next/image"
+import { useForm } from "react-hook-form"
+import { signIn } from 'next-auth/react';
 
 export default function SignIn() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        signIn('credentials', {
+            redirect: false,
+            ...data
+        })
+    }
+
+    console.log(errors)
+
     return <Card.Body className="pb-0">
         <Stack className='flex-row column-gap-3 mb-3 text-muted'>
             <i className="bi bi-arrow-right"></i>
             Iniciar sesion
         </Stack>
-        <Stack className="row-gap-3">
-            <InputGroup>
-                <InputGroup.Text id="basic-addon1" className="border-0"><i className="bi bi-person"></i></InputGroup.Text>
-                <Form.Control
-                    placeholder="Correo electronico"
-                    aria-label="Correo electronico"
-                    aria-describedby="basic-addon1"
-                    className="border-0 bg-secondary-subtle"
-                />
-            </InputGroup>
+        <Form autoComplete="off">
+            <Stack className="row-gap-3">
+                <InputGroup>
+                    <InputGroup.Text id="basic-addon1" className="border-0"><i className="bi bi-person"></i></InputGroup.Text>
+                    <Form.Control
+                        placeholder="Correo electronico"
+                        {...register("email", {
+                            required: "Campo obligatorio",
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "No es un correo válido",
+                            },
+                        })}
+                        aria-label="Correo electronico"
+                        aria-describedby="basic-addon1"
+                        className="border-0 bg-secondary-subtle"
+                        isInvalid={errors.email?.message}
+                    />
+                    <Form.Control.Feedback tooltip type={errors.email?.message && "invalid"}>
+                        {errors.email?.message}
+                    </Form.Control.Feedback>
+                </InputGroup>
 
-            <InputGroup>
-                <InputGroup.Text id="basic-addon1" className="border-0"><i className="bi bi-lock"></i></InputGroup.Text>
-                <Form.Control
-                    placeholder="Contraseña"
-                    aria-label="Contraseña"
-                    aria-describedby="basic-addon1"
-                    className="border-0 bg-secondary-subtle"
-                />
-            </InputGroup>
+                <InputGroup>
+                    <InputGroup.Text id="basic-addon1" className="border-0"><i className="bi bi-lock"></i></InputGroup.Text>
+                    <Form.Control
+                        placeholder="Contraseña"
+                        {...register("pass", {
+                            required: "Campo obligatorio"
+                        })}
+                        aria-label="Contraseña"
+                        aria-describedby="basic-addon1"
+                        className="border-0 bg-secondary-subtle"
+                    />
+                </InputGroup>
 
-            <div className="d-grid row-gap-3">
-                <Link href="" className='text-decoration-none ms-auto text-muted'>
-                    ¿Olvidó su contraseña?
-                </Link>
-                <Button>
-                    Iniciar sesion
-                </Button>
-                <Link href="/signUp" legacyBehavior>
-                    <Button variant="outline-primary">
-                        Crear cuenta
+                <div className="d-grid row-gap-3">
+                    <Link href="" className='text-decoration-none ms-auto text-muted'>
+                        ¿Olvidó su contraseña?
+                    </Link>
+                    <Button
+                        onClick={handleSubmit(onSubmit)}
+                    >
+                        Iniciar sesion
                     </Button>
-                </Link>
-            </div>
-        </Stack>
+                    <Link href="/signUp" legacyBehavior>
+                        <Button variant="outline-primary">
+                            Crear cuenta
+                        </Button>
+                    </Link>
+                </div>
+            </Stack>
+        </Form>
         <Stack direction="horizontal" className="column-gap-3 justify-content-center mt-3 text-muted">
             Acceder mas rapido con:
         </Stack>
