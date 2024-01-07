53b05a3a1ca2d11dd2bde85153ebde9113e8a727
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2024-01-07 14:04:16 -0400

    Adding callbacks and user signin functionality

--- app/(auth)/api/auth/[...nextauth]/route.js
+++ app/(auth)/api/auth/[...nextauth]/route.js
@@ -1,14 +1,62 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { conn } from "@/app/libs/dbC";
+import { uHashP } from "@/app/libs/crypt";
+import { sign } from "@/app/libs/jwtF";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     Credentials({
       async authorize(credentials) {
-        console.log(credentials);
+        const { email, pass } = credentials;
+
+        const selectUser = await conn.query(
+          "SELECT * FROM user WHERE email = ?",
+          email
+        );
+
+        await conn.end();
+
+        if (selectUser.length > 0) {
+          const data = selectUser[0];
+          const hashedPass = data?.pass;
+          const checkPass = await uHashP(pass, hashedPass);
+          if (checkPass) {
+            delete data.pass;
+            const accessToken = sign(data);
+            return {
+              ...data,
+              accessToken,
+            };
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
       },
     }),
   ],
-});
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user;
+        token.accessToken = user.accessToken;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token.user) session.user = token.user;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  secret: process.env.AUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
