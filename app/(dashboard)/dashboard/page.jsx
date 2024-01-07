a367e409a3a27b2f297669aef0cc11cf709563c7
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2024-01-07 14:05:49 -0400

    Checking user session

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,3 +1,7 @@
-export default function Dashboard() {
-    return <span>Dashboard</span>
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/(auth)/api/auth/[...nextauth]/route"
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions)
+    console.log(session)
+    return <span>Hi {session.user.firstName}</span>
 }
