9e6ae3e0c6bf006df384428a62ff2955de1ec8bf
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2024-01-07 15:07:03 -0400

    Removing console.log and adding ternary operator

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -2,6 +2,5 @@ import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/app/(auth)/api/auth/[...nextauth]/route"
 export default async function Dashboard() {
     const session = await getServerSession(authOptions)
-    console.log(session)
-    return <span>Hi {session.user.firstName}</span>
+    return <span>Hi {session?.user.firstName}</span>
 }
