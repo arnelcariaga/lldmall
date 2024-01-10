090e5dc72ab2d891a8960d94bea5b1b20bd9d115
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2024-01-09 22:06:10 -0400

    Adding SessionProvider so we can use useSession hook

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./global.scss";
 import { Inter } from "next/font/google";
+import MySessionProvider from "./MySessionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-bs-theme="dark">
       <body className={inter.className}>
-        {children}
+        <MySessionProvider>
+          {children}
+        </MySessionProvider>
       </body>
     </html>
   );
