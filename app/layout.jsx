485550bf8f8c7f7354df94a413ac57bdb6c1b9cf
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2024-01-14 23:13:48 -0400

    Adding my persiste gate component

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,17 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./global.scss";
 import { Inter } from "next/font/google";
-import MySessionProvider from "./MySessionProvider";
+import MySessionProvider from '@/app/MySessionProvider';
+import MyReduxProvider from '@/app/MyReduxProvider';
+import MyPersistGate from '@/app/MyPersistGate';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-bs-theme="dark">
-      <body className={inter.className}>
-        <MySessionProvider>
-          {children}
-        </MySessionProvider>
-      </body>
-    </html>
+    <MyReduxProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <MySessionProvider>
+            <MyPersistGate>
+              {children}
+            </MyPersistGate>
+          </MySessionProvider>
+        </body>
+      </html>
+    </MyReduxProvider>
   );
 }
